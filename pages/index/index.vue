b515b374c93c5a9556eda2d5e986f9cf297fb4c9
hope-11 <hope-11@163.com>
2023-06-19 10:47:02 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="content">
+		<view class="wrapper">
+			<view class="title">基础使用</view>
+			<DateTabs :value.sync="value1" @change='onDateTabsChange'></DateTabs>
+			<view class="value">当前日期：{{ value1 }}</view>
+		</view>
+		<view class="wrapper">
+			<view class="title">设置当前日期、开始日期和结束日期</view>
+			<DateTabs :value.sync="value2" startDate="2023-06-01" endDate="2023-06-30"></DateTabs>
+			<view class="value">当前日期：{{ value2 }}</view>
+		</view>
+		<view class="wrapper">
+			<view class="title">设置朴素样式</view>
+			<DateTabs :value.sync="value3" plain></DateTabs>
+			<view class="value">当前日期：{{ value3 }}</view>
+		</view>
+		<view class="wrapper">
+			<view class="title">设置主题色</view>
+			<DateTabs :value.sync="value4" color="red" bgColor="rgba(255, 0, 0, 0.1)"></DateTabs>
+			<view class="value">当前日期：{{ value4 }}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import DateTabs from '@/uni_modules/hope-11-date-tabs/components/hope-11-date-tabs/date-tabs.vue'
+
+	export default {
+		components: {
+			DateTabs,
+		},
+		data() {
+			return {
+				value1: '',
+				value2: '2023-06-06',
+				value3: '',
+				value4: '',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			onDateTabsChange(e) {
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		.wrapper {
+			margin: 20px 0;
+
+			.title {
+				margin-bottom: 10px;
+				font-weight: bold;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+
+			.value {
+				margin-top: 10px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		}
+	}
+</style>
---
+++ uni_modules/hope-11-date-tabs/components/hope-11-date-tabs/date-tabs.vue
@@ -0,0 +1,199 @@
+<template>
+	<view class="date-tabs-container" :style="{backgroundColor: bgColor ? bgColor : ''}">
+		<view class="tabs-wrapper">
+			<scroll-view scroll-x :show-scrollbar="false" :scroll-left="scrollLeft" scroll-with-animation class="scroll-view">
+				<view class="date-wrapper">
+					<view v-for="(item, index) in list" :key="index" class="date-item" @click="onItemClick(index)">
+						<view class="week":style="{color: index === current ? color : ''}">{{ item.w }}</view>
+						<view class="date" :class="{current : index === current}"
+							:style="{backgroundColor: index === current && !plain ? color : '', borderColor: index === current ? color : '', color: index === current && plain ? color : ''}">{{ item.d }}</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+		<view class="calendar-button" @click="onOpenCalendar">
+			<Icons v-if="plain" type="calendar" size="36" :color="color"></Icons>
+			<Icons v-else type="calendar-filled" size="36" :color="color"></Icons>
+		</view>
+
+		<Calendar ref="calendar" :insert="false" :date="pickerValue" :startDate="calendarStartDate" :endDate="calendarEndDate" :color="color"
+			@confirm="onCalendarConfirm"></Calendar>
+	</view>
+</template>
+<script>
+	import dayjs from './dayjs.min.js'
+	import Calendar from './uni-calendar/uni-calendar.vue'
+	import Icons from './uni-icons/uni-icons.vue'
+
+	export default {
+		name: "DateTabs",
+		components: {
+			Calendar,
+			Icons,
+		},
+		props: {
+			value: {
+				type: String,
+				default: ''
+			},
+			startDate: {
+				type: String,
+				default: ''
+			},
+			endDate: {
+				type: String,
+				default: ''
+			},
+			color: {
+				type: String,
+				default: '#007aff'
+			},
+			bgColor: {
+				type: String,
+				default: 'white'
+			},
+			plain: {
+				type: Boolean,
+				default: false
+			}
+		},
+		data() {
+			return {
+				pickerValue: '',
+				list: [],
+				current: 0,
+				scrollLeft: 0,
+				dateItemWidth: 0,
+				weekdays: ['日', '一', '二', '三', '四', '五', '六'],
+			}
+		},
+		computed: {
+			calendarStartDate() {
+				return this.startDate || dayjs().format('YYYY-MM-DD')
+			},
+			calendarEndDate() {
+				return this.endDate || dayjs(this.calendarStartDate).add(27, 'd').format('YYYY-MM-DD')
+			}
+		},
+		watch: {
+			dateItemWidth(newVal, oldVal) {
+				for (let i = 0; i < this.list.length; i++) {
+					if (this.list[i].dd === this.value) {
+						this.scrollLeft = this.dateItemWidth * i + Math.random()
+						break
+					}
+				}
+			}
+		},
+		created() {
+			this.initList()
+		},
+		mounted() {
+			const query = uni.createSelectorQuery().in(this)
+			query.select('.date-item').boundingClientRect(res => {
+				this.dateItemWidth = res.width
+			}).exec()
+		},
+		methods: {
+			initList() {
+				const length = dayjs(this.calendarEndDate).diff(this.calendarStartDate, 'day')
+				for (let i = 0; i <= length; i++) {
+					const date = dayjs(this.calendarStartDate).add(i, 'd')
+					this.list.push({
+						date: date.toDate(),
+						dd: date.format('YYYY-MM-DD'),
+						d: date.format('D'),
+						w: i === 0 ? '今' : (date.format('D') === '1' ? date.format('M月') : this.weekdays[date.day()])
+					})
+				}
+				this.onCalendarConfirm({
+					fulldate: this.value || dayjs().format('YYYY-MM-DD')
+				})
+			},
+			onItemClick(index) {
+				this.current = index
+				this.$emit('update:value', this.list[this.current].dd)
+				this.$emit('change', this.list[this.current])
+			},
+			onOpenCalendar() {
+				this.pickerValue = this.list[this.current].dd
+				this.$refs.calendar.open()
+			},
+			onCalendarConfirm(e) {
+				for (let i = 0; i < this.list.length; i++) {
+					if (this.list[i].dd === e.fulldate) {
+						this.onItemClick(i)
+						this.scrollLeft = this.dateItemWidth * i + Math.random()
+						break
+					}
+				}
+			}
+		}
+	}
+</script>
+<style lang="scss">
+	.date-tabs-container {
+		width: 100vw;
+		height: 120rpx;
+		box-shadow: 0 10rpx 10rpx $uni-bg-color-grey;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.tabs-wrapper {
+			width: calc(100% - 120rpx);
+
+			scroll-view {
+				height: 100%;
+
+				.date-wrapper {
+					display: flex;
+
+					.date-item {
+						height: 120rpx;
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						align-items: center;
+
+						view {
+							width: 60rpx;
+							margin: 5rpx 20rpx;
+							display: flex;
+							justify-content: center;
+							align-items: center;
+						}
+
+						.week {
+							font-size: 24rpx;
+							color: grey;
+						}
+
+						.date {
+							height: 60rpx;
+							white-space: nowrap;
+						}
+
+						.current {
+							box-sizing: border-box;
+							border-width: 2px;
+							border-style: solid;
+							border-radius: 50%;
+							color: white;
+							font-weight: bold;
+						}
+					}
+				}
+			}
+		}
+
+		.calendar-button {
+			width: 120rpx;
+			height: 100%;
+			box-shadow: -10rpx 0 10rpx $uni-bg-color-grey;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+</style>
